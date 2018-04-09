6ffd831074b7774916874699f5d89c6ccfeda894
Sweatysven <devanhansendk@gmail.com>
2018-04-09 22:26:32 +0200

    Another Juan1

---
+++ js/explore.js
@@ -0,0 +1,36 @@
+
+function explore() {
+
+    (function () {
+       'use strict';
+     
+          // Isotope filter
+        $(window).load(function() {
+            var $container = $('.web-items');
+            $container.isotope({
+                filter: '*',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            $('.cat a').click(function() {
+                $('.cat .active').removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+        });
+    }());
+}
+
+explore();
